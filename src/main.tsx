c15a95024c7c6675481b92fd0d6368195c8c501c
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-27 18:28:16 -0300

    feat: carregar validação de contrato estrito

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './content/validateContinuousResource';
 import './content/validateContinuousResponse';
 import './content/validateContinuousReturn';
 import './content/validateContinuousShare';
+import './content/validateContinuousStrictContract';
 import './content/validateContinuousTextVisibility';
 import './content/validateContinuousTheme';
 import './content/validateContinuousThemeCycle';
