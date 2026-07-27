fe91dba8d1e41166950edac8b7f33153d3247d72
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-27 08:28:52 -0300

    feat(routes): adicionar rota do ciclo temático

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,7 @@ import { CharacterPage } from '../pages/CharacterPage';
 import { ClassificationPage } from '../pages/ClassificationPage';
 import { CodexPage } from '../pages/CodexPage';
 import { ContinuousCyclePage } from '../pages/ContinuousCyclePage';
+import { ContinuousThemeCyclePage } from '../pages/ContinuousThemeCyclePage';
 import { ContinuousTrailPage } from '../pages/ContinuousTrailPage';
 import { CraftingPage } from '../pages/CraftingPage';
 import { DevPage } from '../pages/DevPage';
@@ -95,6 +96,7 @@ export function App() {
       <Route path="/temple/new-work" element={<NewWorkPage/>}/>
       <Route path="/temple/continuous-cycles" element={<ContinuousCyclePage/>}/>
       <Route path="/temple/continuous-cycles/:instanceId/trail" element={<ContinuousTrailPage/>}/>
+      <Route path="/temple/continuous-cycles/:instanceId/trail/theme-cycle" element={<ContinuousThemeCyclePage/>}/>
       <Route path="/mission/thread-that-gathers" element={<SpiritThreadPage/>}/>
       <Route path="/mission/center-without-erasing-parts" element={<SpiritCenterPage/>}/>
       <Route path="/mission/center-without-erasing" element={<SpiritCenterPage/>}/>
