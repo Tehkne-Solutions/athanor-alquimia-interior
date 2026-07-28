05c4b7a5b34b17f4647ec0d260754caf3f7dc314
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-27 22:03:46 -0300

    test: incluir compatibilidade na validação editorial

--- scripts/validate-content.ts
+++ scripts/validate-content.ts
@@ -23,6 +23,7 @@ try {
   await import('../src/content/validateContinuousExactText');
   await import('../src/content/validateContinuousExactTime');
   await import('../src/content/validateContinuousExactRelation');
+  await import('../src/content/validateContinuousFieldCompatibility');
   console.log('Conteúdo Athanor validado com sucesso.');
 } catch (error) {
   console.error('Falha na validação editorial:');
