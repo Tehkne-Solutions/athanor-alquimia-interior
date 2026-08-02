b201b2c43d98ec5a7a6dc642f00378b318a5db48
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-02 19:29:31 -0300

    test(config): remover duplicacao entre vite e vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 4173 },
-  preview: { port: 4173 },
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    css: true
-  }
+  preview: { port: 4173 }
 });
